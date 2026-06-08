2c107ca6cf428ebe51a3704d1c5d39ada8906909
lourdess8 <lourdessofia1107@gmail.com>
2026-06-08 17:21:00 -0300

    runnerpt2

---
+++ jogos.gd
@@ -0,0 +1,19 @@
+extends Node2D
+@export var velocidade: int
+
+func _ready() -> void:
+	velocidade = 400
+
+func _process(delta: float) -> void:
+	$fundo.position.x -= velocidade*delta*0.5
+	$fundo2.position.x -= velocidade*delta*0.5
+	if $fundo.position.x < -1152: 
+		$fundo.position.x = 1152 
+	if $fundo2.position.x < -1152:
+		$fundo2.position.x = 1152
+	$chao.position.x -= velocidade*delta*2
+	$chao2.position.x -= velocidade*delta*2
+	if $chao.position.x < -1152:
+		$chao.position.x = 1152
+	if $chao2.position.x < -1152:
+		$chao2.position.x = 1152
